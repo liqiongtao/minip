efc1d6df4ddbb0078b2e17de9254faad184fe130
liqiongtao <hnatao@126.com>
2024-11-16 22:11:35 +0800

    bugfix

---
+++ src/pages/index/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <view class="index">
+        <nut-button type="primary" @click="onClick">按钮</nut-button>
+        <nut-toast v-model:visible="show" msg="你成功了" />
+    </view>
+</template>
+
+<script setup>
+    const show = ref(false)
+    const onClick = () => {
+        show.value = true
+    }
+</script>
