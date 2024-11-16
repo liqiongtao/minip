865d8b3c2e4dcf0ef725911001a61251d24898dd
liqiongtao <hnatao@126.com>
2024-11-17 01:26:30 +0800

    bugfix

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,9 @@
 export default {
     env: {
-        NODE_ENV: '"production"'
+        NODE_ENV: '"production"',
+        APP_ID: `"${process.env.TARO_APP_ID}"`,
+        API_BASE_URL: `"${process.env.TARO_APP_API_BASE_URL}"`,
+        API_SECRET_KEY: `"${process.env.TARO_APP_API_SECRET_KEY}"`,
+        API_SECRET_SECRET: `"${process.env.TARO_APP_API_SECRET_SECRET}"`
     }
 }
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,6 +3,7 @@
         <nut-button type="primary" @tap="onClick">按钮</nut-button>
         <nut-toast v-model:visible="show" msg="123" />
         <view>{{ appId }}</view>
+        <view>{{ apiBaseUrl }}</view>
     </view>
 </template>
 
@@ -15,5 +16,6 @@
         // showToast('你点击了')
     }
 
-    const appId = ref(process.env.TARO_APP_ID)
+    const appId = ref(process.env.APP_ID)
+    const apiBaseUrl = ref(process.env.API_BASE_URL)
 </script>
